2e7778c7ea2c263fb61eeeb517aa7031c5f3645e
Ashfaq Hussain <asfak@vcbay.co>
2025-04-22 21:27:54 +0530

    current tab url and title

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [url, setUrl] = useState('')
+  const [title, setTitle] = useState('')
+
+  useEffect(() => {
+    chrome.tabs?.query({ active: true, currentWindow: true }, (tabs) => {
+      console.log('tabs', tabs)
+      if (tabs.length > 0) {
+        setUrl(tabs[0].url || '')
+        setTitle(tabs[0].title || '')
+      }
+    })
+  }, [])
 
   return (
     <div>
-      <div>
-        🔍 Page Info
-      </div>
-      <p className='text-xl'><strong>URL:</strong> {window.location.href}</p>
-      <p><strong>Title:</strong> {document.title}</p>
+      <h2>Current Tab Info</h2>
+      <p><strong>Title:</strong> {title}</p>
+      <p><strong>URL:</strong> {url}</p>
     </div>
   )
 }
